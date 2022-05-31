cc12950670021c794e71d22b5f04d5782e9950b1
Lucas Santos <39504808+lsantoss@users.noreply.github.com>
2022-05-31 12:28:05 -0300

    Renomeando pastas

---
+++ POC.Ajax/POC.Ajax/wwwroot/js/Customers/Models/Customer.js
@@ -0,0 +1,15 @@
+﻿export default class Customer {
+    constructor(id, name, birth, gender) {
+        this.id = id;
+        this.name = name;
+        this.birth = birth;
+        this.gender = gender;
+    }
+
+    constructor(name, birth, gender) {
+        this.id = 0;
+        this.name = name;
+        this.birth = birth;
+        this.gender = gender;
+    }
+}
